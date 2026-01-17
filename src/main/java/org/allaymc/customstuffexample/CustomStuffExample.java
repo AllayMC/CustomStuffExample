19e95cc3dc55a6caed8ea629f5f14a0134be87a9
daoge_cmd <3523206925@qq.com>
2026-01-18 01:00:37 +0800

    feat: not need to create item type manually

--- src/main/java/org/allaymc/customstuffexample/CustomStuffExample.java
+++ src/main/java/org/allaymc/customstuffexample/CustomStuffExample.java
@@ -3,8 +3,6 @@ package org.allaymc.customstuffexample;
 import org.allaymc.api.block.data.BlockStateData;
 import org.allaymc.api.block.type.BlockType;
 import org.allaymc.api.item.creative.CreativeItemCategory;
-import org.allaymc.api.item.data.ItemData;
-import org.allaymc.api.item.type.ItemType;
 import org.allaymc.api.math.voxelshape.VoxelShape;
 import org.allaymc.api.plugin.Plugin;
 import org.allaymc.api.registry.Registries;
@@ -12,34 +10,15 @@ import org.allaymc.customstuffexample.block.BlockTestBlockBehavior;
 import org.allaymc.server.block.component.BlockStateDataComponentImpl;
 import org.allaymc.server.block.type.AllayBlockType;
 import org.allaymc.server.block.type.CustomBlockDefinitionGenerator;
-import org.allaymc.server.item.impl.ItemBlockImpl;
-import org.allaymc.server.item.type.AllayItemType;
-import org.allaymc.server.item.type.CustomItemDefinitionGenerator;
 
 import java.util.Map;
 
 public class CustomStuffExample extends Plugin {
 
-    public static ItemType<ItemBlockImpl> TEST_BLOCK_ITEM;
-
     public static BlockType<BlockTestBlockBehavior> TEST_BLOCK;
 
     @Override
     public void onLoad() {
-        TEST_BLOCK_ITEM = AllayItemType
-                .builder(ItemBlockImpl.class)
-                .identifier("cse:test_block")
-                .itemData(ItemData.DEFAULT)
-                .itemDefinitionGenerator(CustomItemDefinitionGenerator.builder()
-                        .texture("test_block")
-                        .build()
-                )
-                .build();
-        var group = Registries.CREATIVE_ITEMS
-                .getCategory(CreativeItemCategory.Type.CONSTRUCTION)
-                .registerGroup("cse:item_group", TEST_BLOCK_ITEM.createItemStack());
-        group.registerItem(TEST_BLOCK_ITEM.createItemStack());
-
         var shape = VoxelShape.builder()
                 .solid(0, 0, 0, 1, 0.0625, 1)
                 .build();
@@ -63,6 +42,11 @@ public class CustomStuffExample extends Plugin {
                         .build()
                 )
                 .build();
+
+        var group = Registries.CREATIVE_ITEMS
+                .getCategory(CreativeItemCategory.Type.CONSTRUCTION)
+                .registerGroup("cse:item_group", TEST_BLOCK.getItemType().createItemStack());
+        group.registerItem(TEST_BLOCK.getItemType().createItemStack());
 
         this.pluginLogger.info("CustomStuffExample is loaded!");
     }
