d8375d82fa9bb2ced2964841ebd7f95e0ccc0ff6
daoge_cmd <3523206925@qq.com>
2026-01-18 05:05:38 +0800

    feat: update api usage

--- src/main/java/org/allaymc/customstuffexample/CustomStuffExample.java
+++ src/main/java/org/allaymc/customstuffexample/CustomStuffExample.java
@@ -1,24 +1,28 @@
 package org.allaymc.customstuffexample;
 
+import org.allaymc.api.block.BlockBehavior;
 import org.allaymc.api.block.data.BlockStateData;
 import org.allaymc.api.block.type.BlockType;
 import org.allaymc.api.item.creative.CreativeItemCategory;
 import org.allaymc.api.math.voxelshape.VoxelShape;
 import org.allaymc.api.plugin.Plugin;
 import org.allaymc.api.registry.Registries;
-import org.allaymc.customstuffexample.block.BlockTestBlockBehavior;
 import org.allaymc.server.block.component.BlockStateDataComponentImpl;
+import org.allaymc.server.block.impl.BlockBehaviorImpl;
 import org.allaymc.server.block.type.AllayBlockType;
 import org.allaymc.server.block.type.CustomBlockDefinitionGenerator;
 
-import java.util.Map;
-
 public class CustomStuffExample extends Plugin {
 
-    public static BlockType<BlockTestBlockBehavior> TEST_BLOCK;
+    public static BlockType<BlockBehavior> TEST_BLOCK;
 
     @Override
     public void onLoad() {
+        registerBlockTypes();
+        registerCreativeItems();
+    }
+
+    protected void registerBlockTypes() {
         var shape = VoxelShape.builder()
                 .solid(0, 0, 0, 1, 0.0625, 1)
                 .build();
@@ -29,7 +33,7 @@ public class CustomStuffExample extends Plugin {
                 .faceDimming(true)
                 .build();
         TEST_BLOCK = AllayBlockType
-                .builder(BlockTestBlockBehavior.class)
+                .builder(BlockBehaviorImpl.class)
                 .identifier("cse:test_block")
                 .addComponent(BlockStateDataComponentImpl.ofGlobalStatic(BlockStateData.builder()
                         .collisionShape(shape)
@@ -37,27 +41,17 @@ public class CustomStuffExample extends Plugin {
                         .build()
                 ))
                 .blockDefinitionGenerator(CustomBlockDefinitionGenerator.builder()
-                        .materialInstances(Map.of("*", material))
+                        .materials(CustomBlockDefinitionGenerator.Materials.builder().any(material))
                         .geometry("geometry.test_block")
                         .build()
                 )
                 .build();
+    }
 
+    protected void registerCreativeItems() {
         var group = Registries.CREATIVE_ITEMS
                 .getCategory(CreativeItemCategory.Type.CONSTRUCTION)
                 .registerGroup("cse:item_group", TEST_BLOCK.getItemType().createItemStack());
         group.registerItem(TEST_BLOCK.getItemType().createItemStack());
-
-        this.pluginLogger.info("CustomStuffExample is loaded!");
-    }
-
-    @Override
-    public void onEnable() {
-        this.pluginLogger.info("CustomStuffExample is enabled!");
-    }
-
-    @Override
-    public void onDisable() {
-        this.pluginLogger.info("CustomStuffExample is disabled!");
     }
 }
